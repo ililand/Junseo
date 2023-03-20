c2620ff11cc2df3856b6bc5d1358042810711c3c
unknown <dbwnstj628@naver.com>
2023-03-20 19:36:43 +0900

    test

---
+++ Untitled2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	scanf("%d %d",&a,&b);
+	if(c==(90-a)%5==0){
+	
+	printf("%d",b+c);
+	}
+	else if((c+1)==(90-a)%5!=0){
+	
+		printf("%d",b+c+1);0
+			
+	}
+
+}
---
+++ Untitled4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+	int i; 
+	scanf("%d",&i);
+		for(int j=10;j>0;j--){
+	printf("%d * %d = %d\n",i,j,i*j);		}
+	
+	
+	return 0;
+}
